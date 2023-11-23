db12a15d8d2781487005661dfcfa78fd7b09ce9b
IzzIzzIzzIzzI <iskoa002@osloskolen.no>
2023-11-23 09:33:11 +0100

    Made gradient background and started on navbar

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<div class="back"></div>
+
+<style>
+    .back {
+        width: 100%;
+        height: 150px;
+        background-color: #3c144b;
+        position: fixed;
+        border-radius: 25px;
+        top: -50px;
+    }
+
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Navbar from "$lib/components/navbar.svelte";
+
+</script>
+
+<div class="background"></div>
+<Navbar/>
+
+
+<style>
+    .background {
+        height: 100%;
+        width: 100%;
+        background-image: linear-gradient(#945aaa, #5a5aaa);
+        position: fixed;
+    }
+
+</style>
