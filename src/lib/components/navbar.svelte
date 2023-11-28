6d91323313c07f96bf54cd5ec7ba0edceb6ab8a5
IzzIzzIzzIzzI <iskoa002@osloskolen.no>
2023-11-28 14:15:23 +0100

    navbar

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -2,9 +2,44 @@
 
 </script>
 
-<div class="back"></div>
+<link rel="stylesheet" href="https://use.typekit.net/wmh1kbd.css">
+<link rel="stylesheet" href="https://use.typekit.net/wmh1kbd.css">
+
+<div class="back">
+    <div class="mooncon">
+        <h1>Mooncon</h1>
+    </div>
+    <div class="options">
+        <button class="about_us">
+            <h2>About us</h2>
+        </button>
+        <button class="FAQ">
+            <h2>FAQ</h2>
+        </button>
+        <button class="competitions">
+            <h2>Competitions</h2>
+        </button>
+        <button class="activities">
+            <h2>Activities</h2>
+        </button>
+        <button class="info">
+            <h2>Info</h2>
+        </button>
+        <button class="tickets">
+            <h2>Tickets</h2>
+        </button>
+    </div>
+</div>
 
 <style>
+    button {
+        border: none;
+        color: white;
+        background-color: rgba(0, 0, 0, 0);
+        height: fit-content;
+        width: fit-content;
+    }
+
     .back {
         width: 100%;
         height: 150px;
@@ -14,4 +49,25 @@
         top: -50px;
     }
 
+    .mooncon {
+        color: white;
+        right: 0;
+        top: 0;
+        position: fixed;
+        font-family: rafaella-shadow;
+        font-weight: 700;
+    } .mooncon h1 {
+        font-size: 50px;
+        margin: 25px;
+    }
+
+    .options {
+        position: fixed;
+        top: 5px;
+        height: 100px;
+        display: flex;
+        
+    }
+
 </style>
+
