30b57d853c92424b94c26a5e684930cf1c6533b3
IzzIzzIzzIzzI <iskoa002@osloskolen.no>
2024-01-09 14:31:29 +0100

    Started the 'tickets' page

---
+++ src/routes/tickets/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import logo from "$lib/pictures/Mooncon_logo.png";
+</script>
+
+<link rel="stylesheet" href="https://use.typekit.net/wmh1kbd.css">
+<link rel="stylesheet" href="https://use.typekit.net/wmh1kbd.css">
+<link rel="stylesheet" href="https://use.typekit.net/wmh1kbd.css">
+
+<div class="title">
+    
+</div>
+
+<style>
+    .title {
+        width: 100px;
+        height: 100px;
+        background-color: blueviolet;
+        top: 100px;
+        position: absolute;
+    }
+</style>
