3d25f9fdada02e985791271d8260cbeb56521343
IzzIzzIzzIzzI <iskoa002@osloskolen.no>
2024-01-09 15:07:33 +0100

    ye

--- src/routes/tickets/+page.svelte
+++ src/routes/tickets/+page.svelte
@@ -6,16 +6,49 @@
 <link rel="stylesheet" href="https://use.typekit.net/wmh1kbd.css">
 <link rel="stylesheet" href="https://use.typekit.net/wmh1kbd.css">
 
-<div class="title">
-    
+<div class="top">
+    <div class="title">
+        <h1>Tickets</h1>
+    </div>
+    <div class="line">
+
+    </div>
+</div>
+
+<div class="logo">
+    <img src="{logo}" alt="Mooncon logo">
 </div>
 
 <style>
-    .title {
-        width: 100px;
-        height: 100px;
-        background-color: blueviolet;
+    .top {
+        width: 100vw;
+        height: 110px;
         top: 100px;
-        position: absolute;
+        position: relative;
+        /* background-color: blue; */
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .title {
+        width: 100vw;
+        height: 100px;
+        /* background-color: blueviolet; */
+        justify-self: center;
+        align-items: center;
+        justify-content: center;
+        display: flex;
+    } .title h1 {
+        display: flex;
+        font-family: latienne-Pro-Bold;
+        color: white;
+        font-size: 50px;
+    }
+
+    .line {
+        width: 85vw;
+        height: 5px;
+        background-color: #3c144b;
     }
 </style>
