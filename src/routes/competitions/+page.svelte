726157fcfe43779382d828492680bcf483436448
IzzIzzIzzIzzI <iskoa002@osloskolen.no>
2024-01-25 10:05:35 +0100

    E

--- src/routes/competitions/+page.svelte
+++ src/routes/competitions/+page.svelte
@@ -15,21 +15,24 @@
     </div>
 </div>
 
-<div class="cosplay">
-    <div class="main_cosplay">
+<div class="main">
+    <div class="cosplay">
+        <div class="main_cosplay">
+            <h1>Cosplay competition</h1>
+            <p>Lorem ipsum dolor sit amet. Id nisi illo aut voluptatum similique qui rerum officiis! Ea recusandae placeat ut repellat cupiditate At molestiae earum ad nesciunt autem est provident sint. Est culpa earum et fugit ullam non omnis ipsum et corrupti earum At sunt voluptatum. Et laudantium culpa ad commodi voluptas sit voluptas totam est amet nisi ex enim labore qui assumenda ipsum non velit sapiente. Lorem ipsum dolor sit amet. Id nisi illo aut voluptatum similique qui rerum officiis!</p>
+        </div>
 
-    </div>
+        <div class="div_cosplay">
 
-    <div class="div_cosplay">
+        </div>
 
-    </div>
+        <div class="info_cosplay">
 
-    <div class="info_cosplay">
+        </div>
 
-    </div>
+        <div class="line_cosplay">
 
-    <div class="line_cosplay">
-
+        </div>
     </div>
 </div>
 
@@ -64,13 +67,32 @@
         background-color: #3c144b;
     }
 
+    .main {
+        width: 100vw;
+        top: 200px;
+        display: flex;
+        justify-content: center;
+        position: absolute;
+    }
+
     .cosplay {
         width: 80vw;
         height: 30vw;
-        top: 200px;
-        position: absolute;
-        background-color: black;
+        position: relative;
         display: flex;
-        
+        flex-direction: row;
+    }
+
+    .main_cosplay {
+        width: 50vw;
+        display: flex;
+        flex-direction: column;
+        color: white;
+        font-family: rockwell-nova;
+        background-color: blueviolet;
+        font-size: 20px;
+    } .main_cosplay h1 {
+        font-family: latienne-pro;
+        background-color: aqua;
     }
 </style>
