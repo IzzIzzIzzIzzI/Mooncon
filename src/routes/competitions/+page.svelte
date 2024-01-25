d22155674bc982b3c179b82aaae4183be12fc592
IzzIzzIzzIzzI <iskoa002@osloskolen.no>
2024-01-25 08:39:50 +0100

    E

---
+++ src/routes/competitions/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<link rel="stylesheet" href="https://use.typekit.net/wmh1kbd.css">
+<link rel="stylesheet" href="https://use.typekit.net/wmh1kbd.css">
+<link rel="stylesheet" href="https://use.typekit.net/wmh1kbd.css">
+
+<div class="test">
+
+</div>
+
+<style>
+    .test {
+        width: 20vw;
+        height: 20vw;
+        top: 100px;
+        position: fixed;
+        background-color: black;
+    }
+</style>
