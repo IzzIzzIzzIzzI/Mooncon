e9f6f6436c58f63ef1983600b49778ad00a2ad5b
IzzIzzIzzIzzI <iskoa002@osloskolen.no>
2024-02-08 09:15:32 +0100

    started on the FAQ page

---
+++ src/routes/faq/+page.svelte
@@ -0,0 +1,48 @@
+<script>
+
+</script>
+
+<link rel="stylesheet" href="https://use.typekit.net/wmh1kbd.css">
+<link rel="stylesheet" href="https://use.typekit.net/wmh1kbd.css">
+<link rel="stylesheet" href="https://use.typekit.net/wmh1kbd.css">
+
+<div class="top">
+    <div class="title">
+        <h1>FAQ</h1>
+    </div>
+
+    <div class="line_main">
+    </div>
+</div>
+
+<style>
+    .top {
+        width: 100vw;
+        height: 160px;
+        top: 90px;
+        position: absolute;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .title {
+        width: 100vw;
+        height: 80px;
+        justify-self: center;
+        align-items: center;
+        justify-content: center;
+        display: flex;
+    } .title h1 {
+        display: flex;
+        font-family: latienne-Pro-Bold;
+        color: white;
+        font-size: 50px;
+    }
+
+    .line_main {
+        width: 85vw;
+        height: 5px;
+        background-color: #3c144b;
+    }
+</style>
