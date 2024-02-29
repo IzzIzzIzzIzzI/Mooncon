620e756d5b3bd620037db2efff1227032920ec86
IzzIzzIzzIzzI <iskoa002@osloskolen.no>
2024-02-29 10:55:33 +0100

    E

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -15,6 +15,14 @@
     </div>
 </div>
 
+<div class="main">
+    <h1 class="sub_titel">Rules</h1>
+
+    <p class="text">Lorem ipsum dolor sit amet. Id nisi illo aut voluptatum similique qui rerum officiis! Ea recusandae placeat ut repellat cupiditate At molestiae earum ad nesciunt autem est provident sint. Est culpa earum et fugit ullam non omnis ipsum et corrupti earum At sunt voluptatum. Et laudantium culpa ad commodi voluptas sit voluptas totam est amet nisi ex enim labore qui assumenda ipsum non velit sapiente.Lorem ipsum dolor sit amet. Id nisi illo aut voluptatum similique qui rerum officiis!</p>
+
+    <div class="line_horizontal"></div>
+</div>
+
 <style>
     .top {
         width: 100vw;
@@ -45,5 +53,35 @@
         width: 85vw;
         height: 5px;
         background-color: #3c144b;
+    }
+
+    .main {
+        width: 100vw;
+        top: 200px;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        position: absolute;
+        color: white;
+    } .main h1 {
+        width: 80vw;
+        margin-bottom: 0px;
+        font-family: latienne-pro;
+        font-style: medium;
+    } .main p {
+        width: 80vw;
+        margin-top: 0px;
+        font-family: rockwell-nova;
+        font-style: regular;
+        font-size: 20px;
+    }
+
+    .line_horizontal {
+        width: 85vw;
+        height: 5px;
+        margin-top: 20px;
+        margin-bottom: 20px;
+        background-color: #3c144b;
+        opacity: 60%;
     }
 </style>
