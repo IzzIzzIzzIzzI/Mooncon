4f3db8c82fc28ba981dd9a298447e4baea53bdd1
IzzIzzIzzIzzI <iskoa002@osloskolen.no>
2024-03-19 14:47:52 +0100

    Did the about us page

---
+++ src/routes/about_us/+page.svelte
@@ -0,0 +1,85 @@
+<script>
+    import logo from "$lib/pictures/Mooncon_logo.png";
+</script>
+
+<link rel="stylesheet" href="https://use.typekit.net/wmh1kbd.css">
+<link rel="stylesheet" href="https://use.typekit.net/wmh1kbd.css">
+<link rel="stylesheet" href="https://use.typekit.net/wmh1kbd.css">
+
+<div class="top">
+    <div class="title">
+        <h1>About us</h1>
+    </div>
+
+    <div class="line_main">
+    </div>
+</div>
+
+<div class="main">
+    <div class="text">
+        <p>Heya! We're just like your standard cosplay convention, the main diffrence is that we're fake! Lorem ipsum dolor sit amet. Id nisi illo aut voluptatum similique qui rerum officiis! Ea recusandae placeat ut repellat cupiditate At molestiae earum ad nesciunt autem est provident sint. Est culpa earum et fugit ullam non omnis ipsum et corrupti earum At sunt voluptatum. Et laudantium culpa ad commodi voluptas sit voluptas totam est amet nisi ex enim labore qui assumenda ipsum non velit sapiente.</p>
+    </div>
+
+    <div class="logo">
+        <img src="{logo}" alt="Clouds infront of Mooncon logo">
+    </div>
+
+</div>
+
+
+<style>
+    .top {
+        width: 100vw;
+        height: 160px;
+        top: 90px;
+        position: absolute;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .title {
+        width: 100vw;
+        height: 80px;
+        justify-self: center;
+        align-items: center;
+        justify-content: center;
+        display: flex;
+    } .title h1 {
+        display: flex;
+        font-family: latienne-Pro;
+        font-style: bold;
+        color: white;
+        font-size: 50px;
+    }
+
+    .line_main {
+        width: 85vw;
+        height: 5px;
+        background-color: #3c144b;
+    }
+
+    .main {
+        width: 100vw;
+        top: 200px;
+        display: flex;
+        justify-content: center;
+        flex-direction: row;
+        position: absolute;
+    }
+
+    .text {
+        color: white;
+        width: 40vw;
+    } .text p {
+        font-size: x-large;
+        font-family: rockwell-nova;
+        font-style: regular;
+    }
+
+    .logo {
+        width: fit-content;
+    } .logo img {
+        width: 40vw;
+    }
+</style>
